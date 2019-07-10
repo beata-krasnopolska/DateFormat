dc47455e44e7ece3bd3f3f4f4815dc3f0dfd395d
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-10 22:04:20 +0200

    Add project files.

---
+++ DataFormat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program retrieves the current date and displays it in different ways");
+
+            DateTime currentDate = DateTime.Today;
+            Console.WriteLine("Today is {0}", currentDate.ToString());
+            Console.WriteLine("Display the date in different formats:");
+            Console.WriteLine(currentDate.ToString("d"));
+            Console.WriteLine(currentDate.ToString("D"));
+            Console.WriteLine(currentDate.ToString("g"));
+
+            Console.ReadKey();
+        }
+    }
+}
